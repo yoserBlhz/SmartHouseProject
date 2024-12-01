7867f5f4e2b18c223bc116f67cdbf6a16df7d0cb
malak <malek.bendhia@fsb.ucar.tn>
2024-12-01 01:58:41 +0100

    page device details done

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+//import { DevicesListComponent } from './components/devices-list/devices-list.component';
+import { TestComponent } from './components/test/test.component';
+export const routes: Routes = [
+
+    {path:'',component:TestComponent}
+];
