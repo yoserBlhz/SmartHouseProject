f7385174be13314fec441a5fb0d44c54f0a72038
yosr <benlehzilyosrclub@gmail.com>
2024-12-02 00:33:31 +0100

    merge

--- frontend/smartHouseFront/src/app/app.routes.ts
+++ frontend/smartHouseFront/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { SignupComponent } from '../components/signup/signup.component';
 import { WelcomePageComponent } from '../components/welcome-page/welcome-page.component';
+import { SigninComponent } from '../components/signin/signin.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent},
     {path: 'home', component: DashboardComponent},
     {path: '',component: WelcomePageComponent},
-    {path: 'signup',component: SignupComponent}
+    {path: 'signup',component: SignupComponent},
+    {path: 'login',component: SigninComponent}
+
 
 ];
