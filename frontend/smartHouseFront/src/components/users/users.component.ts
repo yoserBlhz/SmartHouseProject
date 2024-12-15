ab6673ee42ab01a2671f8eaeeb212e159caf965a
yosr <benlehzilyosrclub@gmail.com>
2024-12-15 15:11:54 +0100

    crud of users

--- frontend/smartHouseFront/src/components/users/users.component.ts
+++ frontend/smartHouseFront/src/components/users/users.component.ts
@@ -1,78 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { NavbarComponent } from "../navbar/navbar.component";
-// import { SidebarComponent } from "../sidebar/sidebar.component";
-// import { CommonModule, NgFor } from '@angular/common';
-// import { AuthService } from '../../Services/auth.service';
-// import { HttpClientModule } from '@angular/common/http';
-// import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { UsersService } from '../../Services/users.service';
-
-// @Component({
-//   selector: 'app-users',
-//   standalone: true,
-//   imports: [NavbarComponent, SidebarComponent,CommonModule,HttpClientModule,FormsModule,ReactiveFormsModule,],
-//   providers: [AuthService,UsersService],  
-//   templateUrl: './users.component.html',
-//   styleUrl: './users.component.scss'
-// })
-// export class UsersComponent  implements OnInit {
-//    userForm!: FormGroup;
-//     userInfo: { code: string, username: string } | null = null;
-// constructor(
-//     private fb: FormBuilder,
-//     private authService: AuthService,
-//     private usersService: UsersService,
-//     private router: Router
-//   ) {}
-
-//  initForm() {
-//       this.userForm = this.fb.group({
-//         name: ['', Validators.required]
-//          });
-//       }
-    
-//       ngOnInit() {
-//         this.initForm();
-//         this.authService.userInfo$.subscribe(userInfo => {
-//           this.userInfo = userInfo;
-//           if (!userInfo) {
-//             this.router.navigate(['/login']);
-//           }
-//         });
-//       }
-
-
-//       saveUser(){
-//         if (this.userForm.valid && this.userInfo) {
-//           const formValue = this.userForm.value;
-//           const userData={
-//             name:formValue.name
-//           };
-//           this.usersService.addUser(this.userInfo.code, this.userInfo.username, userData).subscribe({
-//             next: (response) => {
-//               console.log('User added successfully', response);
-//               document.getElementById('addUserModal')?.classList.remove('show');
-//               document.body.classList.remove('modal-open');
-//               document.querySelector('.modal-backdrop')?.remove();
-//             },
-//             error: (error) => {
-//               console.error('Error adding user', error);
-//             }
-//           });        }
-
-//       }
-
-
-
-
-
-
-
-
-// }
-
-
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
@@ -164,47 +89,90 @@ export class UsersComponent implements OnInit {
   }
 
   editUser(user: any) {
+    console.log('Editing user:', user);
+    this.currentEditingUser = user;
     this.editForm.patchValue({ name: user.username });
-    // Open edit modal
   }
 
- 
-  
   saveEditedUser() {
     if (this.editForm.valid && this.userInfo && this.currentEditingUser) {
+      console.log('Saving edited user:', this.currentEditingUser);
       const formValue = this.editForm.value;
       const userData = {
         name: formValue.name
       };
-      console.log("current id"+this.currentEditingUser._id);
-      this.usersService.editUser(this.userInfo.code, this.userInfo.username, this.currentEditingUser._id, userData).subscribe({
+      this.usersService.editUser(this.userInfo.code, this.userInfo.username, this.currentEditingUser.username, userData).subscribe({
         next: (response) => {
           console.log('User edited successfully', response);
-          document.getElementById('editUserModal')?.classList.remove('show');
-          document.body.classList.remove('modal-open');
-          document.querySelector('.modal-backdrop')?.remove();
+         document.getElementById('editUserModal')?.classList.remove('show');
+         document.body.classList.remove('modal-open');
+        document.querySelector('.modal-backdrop')?.remove();
           this.loadUsers();
           this.currentEditingUser = null;
+          this.editForm.reset();
         },
         error: (error) => {
           console.error('Error editing user', error);
         }
       });
+    } else {
+      console.error('Invalid form or missing user information');
     }
   }
 
-  deleteUser(userId: string) {
-    if (this.userInfo) {
-      this.usersService.deleteUser(this.userInfo.code, this.userInfo.username, userId).subscribe({
+  deleteUser2(userNom: string) {
+    if (this.userInfo && userNom) {
+      if (confirm('Are you sure you want to delete this user?')) {
+        this.usersService.deleteUser(this.userInfo.code, this.userInfo.username, userNom).subscribe({
+          next: (response) => {
+            console.log('User deleted successfully', response);
+            this.loadUsers();
+          },
+          error: (error) => {
+            console.error('Error deleting user', error);
+          }
+        });
+      }
+    } else {
+      console.error('Missing user information or user Name');
+    }
+  }
+
+
+  userToDelete: string | null = null;
+
+  confirmDelete(userNom: string) {
+    this.userToDelete = userNom;
+  }
+
+  deleteUser() {
+    if (this.userInfo && this.userToDelete) {
+      this.usersService.deleteUser(this.userInfo.code, this.userInfo.username, this.userToDelete).subscribe({
         next: (response) => {
           console.log('User deleted successfully', response);
+          document.getElementById('deleteUserModal')?.classList.remove('show');
+          document.body.classList.remove('modal-open');
+         document.querySelector('.modal-backdrop')?.remove();
           this.loadUsers();
+          this.userToDelete = null;
         },
         error: (error) => {
           console.error('Error deleting user', error);
         }
       });
+    } else {
+      console.error('Missing user information or user Name');
     }
   }
+
+
+
+
+
+
+
+
+
+
 }
 
